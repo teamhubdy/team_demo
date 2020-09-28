e504f97bcbb3fc4d3cbb9cf92c7938dfcdeb4bce
DESKTOP-AT1HIJB\ahn <theta93@gmail.com>
2020-09-29 01:01:25 +0900

    service test - join

---
+++ src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -0,0 +1,34 @@
+package com.example.demo.service;
+
+import com.example.demo.domain.Member;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+
+    MemberService memberService = new MemberService();
+
+    @Test
+    void join(){
+        //given
+        Member member = new Member(); 
+        member.setName("hello");
+        //when
+        memberService.join(member);
+        //then
+        Member findMember = memberService.findOne(member.getId()).get();
+        Assertions.assertEquals(member, findMember);
+        System.out.println("join test");
+    }
+
+    @Test
+    void findMembers(){
+
+    }
+
+    @Test
+    void findOne(){
+
+    }
+}
