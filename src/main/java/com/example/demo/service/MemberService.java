516937c9a628a22aa664fc04fe24f301652b217e
DESKTOP-AT1HIJB\ahn <theta93@gmail.com>
2020-09-28 23:50:45 +0900

    service

---
+++ src/main/java/com/example/demo/service/MemberService.java
@@ -0,0 +1,32 @@
+package com.example.demo.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
+
+public class MemberService {
+    
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    public Long join(Member member){
+
+        memberRepository.findByName(member.getName()).ifPresent(m->{
+            throw new IllegalStateException("invalid member name!");
+        });
+        
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOneMember(Long id){
+        return memberRepository.findById(id);
+    }
+}
