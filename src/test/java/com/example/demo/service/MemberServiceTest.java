f40e7e9632bbb46ae852eff75f2af1abdcba844d
DESKTOP-AT1HIJB\ahn <theta93@gmail.com>
2020-10-03 01:18:09 +0900

    meberServiceTest beforeeach

--- src/test/java/com/example/demo/service/MemberServiceTest.java
+++ src/test/java/com/example/demo/service/MemberServiceTest.java
@@ -1,14 +1,31 @@
 package com.example.demo.service;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import com.example.demo.domain.Member;
+import com.example.demo.repository.MemberRepository;
+import com.example.demo.repository.MemoryMemberRepository;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberService();
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    void beforeEach(){
+        memberRepository=new MemoryMemberRepository();
+        memberService=new MemberService(memberRepository);
+    }
+
+    @AfterEach
+    void afterEach(){
+        memberRepository.clearStore();
+    }
 
     @Test
     void join(){
@@ -25,7 +42,20 @@ public class MemberServiceTest {
 
     @Test
     void 중복회원가입(){
+        // given
         Member member1 = new Member();
+        member1.setName("spring1");
+
+        Member member2 = new Member();
+        member2.setName("spring1");
+
+        // when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, ()-> memberService.join(member2));
+        Assertions.assertEquals("invalid member name!", e.getMessage());
+        
+        // then
+
     }
 
     @Test
